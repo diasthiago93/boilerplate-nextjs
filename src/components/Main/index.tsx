3b3951b0c4da76064779b679751109858d27dcd6
Thiago Dias <thiagohd93@gmail.com>
2021-03-04 11:50:45 -0300

    Updates Codes

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,17 @@
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <main>
-       <h1>React Avançado</h1>
-    </main>
+    <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>Boilerplate</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>   
+  </S.Wrapper>
   ); 
 }
 
